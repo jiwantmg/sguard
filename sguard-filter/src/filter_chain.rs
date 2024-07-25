279a783f8da08e540b18874c67f5beb208e0cea1
jiwankumartamang <jita@logpoint.com>
2024-07-25 15:35:47 +0545

    comment updated

--- sguard-filter/src/filter_chain.rs
+++ sguard-filter/src/filter_chain.rs
@@ -15,16 +15,28 @@ impl FilterChain {
 
 impl Filter for FilterChain {
     fn handle(&self, req: &Request<Body>, next: FilterFn) -> FilterRs {
-        // Build the filter chain in reverse order
         let mut next = next.clone();
-
+        // Build the filter chain in reverse order. Building the filter chain in reverse
+        // order is a common pattern in middleware and filter frameworks, particularly in web
+        // servers and processing pipelines. Here’s why this approach is used
+        // Why Build in Reverse Order?
+        // 1. Chain Execution Order:
+        //      a. When you build the chain in reverse order, you ensure that filters are applied
+        //         in the order you want them to be executed.
+        //      b. For example, if you have filters A, B, and C, and you want them to process requests
+        //         in the order A -> B -> C, building the chain in reverse (starting from C and adding
+        //         A last) ensures that requests pass through A first and then proceed to B and C.
+        // 2. Handler Composition:
+        //      a. Each filter typically wraps the next filter in the chain. If you start building the chain
+        //         from the end (i.e., from the filter that should handle the request last), each filter can
+        //         wrap the previous filter's handler.
+        //      b. By the time you finish building the chain, the first filter you added (A) will be the
+        //         outermost filter, and the last filter you added (C) will be the innermost filter.
         for filter in self.filters.iter().rev() {
             let current_next = next.clone();
             let filter = filter.clone();
             next = Arc::new(move |req| filter.handle(req, current_next.clone()));
         }
-
-        // Execute the filter chain
         next(req)
     }
 }
--- sguard-filter/src/session.rs
+++ sguard-filter/src/session.rs
@@ -5,7 +5,7 @@ use hyper::{Body, Request};
 use crate::core::{Filter, FilterFn, FilterRs};
 use crate::filter_chain::FilterChainTrait;
 
-// test commit
+// test commit test commit
 pub trait SessionManagementFilterTrait: FilterChainTrait {
     fn sub_filter_chain(&self) -> Option<Arc<dyn SessionManagementFilterTrait>>;
 }
