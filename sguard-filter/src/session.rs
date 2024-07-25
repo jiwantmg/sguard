4a67c9460d9edce83d9d16a40434b8bcabbf993a
jiwankumartamang <jita@logpoint.com>
2024-07-25 15:12:26 +0545

    test commit

--- sguard-filter/src/session.rs
+++ sguard-filter/src/session.rs
@@ -5,6 +5,7 @@ use hyper::{Body, Request};
 use crate::core::{Filter, FilterFn, FilterRs};
 use crate::filter_chain::FilterChainTrait;
 
+// test commit
 pub trait SessionManagementFilterTrait: FilterChainTrait {
     fn sub_filter_chain(&self) -> Option<Arc<dyn SessionManagementFilterTrait>>;
 }
